1b62935bd924c38f869a99fe2ab65ca7f5b96a17
Georgi1994 <Georgi1994@students.softuni.bg>
2026-04-18 10:23:45 +0300

    Add files via upload

---
+++ LoginResponseDto.cs
@@ -0,0 +1,8 @@
+namespace MovieCatalogApiTests.Dtos;
+
+public class LoginResponseDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string AccessToken { get; set; } = string.Empty;
+}
